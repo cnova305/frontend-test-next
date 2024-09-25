9cac5f8b471eeaa652f0795cc67d0c91613bfd1b
Christopher Juwao <njuwao@icloud.com>
2024-09-25 15:42:19 +0200

    card styling

--- components/speaker-card.tsx
+++ components/speaker-card.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type SpeakerCardProps = {
   name: string;
   company: string;
@@ -14,21 +12,19 @@ export default function SpeakerCard({
   topics,
 }: SpeakerCardProps) {
   return (
-    <div className="flex flex-col rounded-2xl w-96 bg-[#ffffff] shadow-xl">
-      <h2 className="text-3xl font-bold text-[#171717]">{name}</h2>
-      <h3 className="text-2xl font-bold text-[#171717]">{company}</h3>
-      <Image
-        className="w-48 h-48 rounded-2xl mx-auto my-4"
-        src={image}
-        alt={name}
-        width={48}
-        height={48}
-      />
-      {topics.map((topic) => (
-        <p key={topic} className="text-2xl font-bold text-[#171717]">
-          {topic}
-        </p>
-      ))}
+    <div className="flex flex-col justify-end w-full aspect-[12/16] rounded-2xl bg-[url('/img1.png')] bg-cover bg-center shadow-xl p-4">
+      <h2 className="text-3xl font-bold text-white">{name}</h2>
+      <h3 className="text-2xl font-bold text-white">{company}</h3>
+      <div className="flex space-x-4 py-3 ">
+        {topics.map((topic) => (
+          <div
+            key={topic}
+            className="inline-flex items-center justify-center px-4 border border-white rounded-full text-sm font-medium text-white bg-white/10"
+          >
+            {topic}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
