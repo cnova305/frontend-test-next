b35d558ae3d33d5c51cb87ec86cb50b735594b55
Christopher Juwao <njuwao@icloud.com>
2024-09-25 16:13:30 +0200

    mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
-import SpeakerCard from "../components/speaker-card";
+import { SpeakerCardProps } from "@/types";
+import Slider from "../components/slider";
 
-const speaker = {
-  name: "Tim Cook",
-  company: "Apple",
-  image: "img1.png",
-  topics: ["Business", "CEO", "Products"],
-};
+const speakers: SpeakerCardProps[] = [
+  {
+    name: "Tim Cook",
+    company: "Apple",
+    image: "img1.png",
+    topics: ["Business", "CEO", "Products"],
+  },
+  {
+    name: "Susan Wojcicki",
+    company: "Google",
+    image: "img1.png",
+    topics: ["Business", "CEO", "UX"],
+  },
+  {
+    name: "Jack Dorsey",
+    company: "Twitter",
+    image: "img1.png",
+    topics: ["Business", "CEO", "UX"],
+  },
+  {
+    name: "Mark Zuckerberg",
+    company: "Meta",
+    image: "img1.png",
+    topics: ["Business", "CEO", "UX"],
+  },
+];
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SpeakerCard
-          name={speaker.name}
-          company={speaker.company}
-          image={speaker.image}
-          topics={speaker.topics}
-        />
+        <Slider items={speakers} />
       </main>
     </div>
   );
