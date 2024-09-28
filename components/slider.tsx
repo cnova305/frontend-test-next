67e9e221fa2e998fc3c1b891dce79fcae1e3dc81
Christopher Juwao <njuwao@icloud.com>
2024-09-28 13:23:05 +0200

    slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,45 +1,54 @@
-"use client";
-
-import { SpeakerCardProps } from "@/types";
-import { useState } from "react";
+import { Speaker } from "@/types";
+import React, { useState } from "react";
 import Modal from "./modal";
 import SpeakerCard from "./speaker-card";
 
-export default function Slider({ items }: { items: SpeakerCardProps[] }) {
-  const [viewSpeaker, setViewSpeaker] = useState(false);
-  const [selectedSpeaker, setSelectedSpeaker] =
-    useState<SpeakerCardProps | null>(null);
+export default function Slider({ speakers }: { speakers: Speaker[] }) {
+  const [viewModal, setViewModal] = useState<boolean>(false);
+  const [selectedSpeaker, setSelectedSpeaker] = useState<Speaker | null>(null);
 
-  const handleViewSpeaker = (speaker: SpeakerCardProps) => {
+  const speakersLoop = [...speakers, ...speakers];
+
+  const cardImage = (url: string): React.CSSProperties => ({
+    backgroundImage: `url(${url})`,
+  });
+
+  const handleOpenModal = (speaker: Speaker) => {
     setSelectedSpeaker(speaker);
-    setViewSpeaker(true);
+    setViewModal(true);
   };
 
   const handleCloseModal = () => {
-    setViewSpeaker(false);
     setSelectedSpeaker(null);
+    setViewModal(false);
   };
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {items.map((item) => (
-          <SpeakerCard
-            key={item.name}
-            name={item.name}
-            company={item.company}
-            image={item.image}
-            topics={item.topics}
-            onClick={() => handleViewSpeaker(item)}
-          />
-        ))}
+      <div className="slider-container flex w-max">
+        <div
+          className={` ${selectedSpeaker ? "paused" : "animate-scroll-infinite"} space-x-8`}
+        >
+          {speakersLoop.map((speaker, index) => (
+            <div
+              key={index}
+              onClick={() => handleOpenModal(speaker)}
+              style={cardImage(speaker.image)}
+              className="speaker-card aspect-[12/16] min-w-[300px] cursor-pointer bg-cover bg-center p-4 md:min-w-[350px]"
+            >
+              <SpeakerCard speaker={speaker} />
+            </div>
+          ))}
+        </div>
       </div>
 
-      <Modal
-        isOpen={viewSpeaker}
-        onClose={handleCloseModal}
-        speaker={selectedSpeaker!}
-      />
+      {selectedSpeaker && (
+        <Modal
+          isOpen={viewModal}
+          onClose={handleCloseModal}
+          speaker={selectedSpeaker}
+        />
+      )}
     </>
   );
 }
