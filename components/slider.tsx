f2e7a00cca2476c7c4da11a1ccc3086026efcac8
Christopher Juwao <njuwao@icloud.com>
2024-09-30 15:21:49 +0200

    slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,17 +1,12 @@
 import { Speaker } from "@/types";
-import React, { useState } from "react";
+import { useState } from "react";
 import Modal from "./modal";
 import SpeakerCard from "./speaker-card";
 
 export default function Slider({ speakers }: { speakers: Speaker[] }) {
+  const speakersLoop = [...speakers, ...speakers];
   const [viewModal, setViewModal] = useState<boolean>(false);
   const [selectedSpeaker, setSelectedSpeaker] = useState<Speaker | null>(null);
-
-  const speakersLoop = [...speakers, ...speakers];
-
-  const cardImage = (url: string): React.CSSProperties => ({
-    backgroundImage: `url(${url})`,
-  });
 
   const handleOpenModal = (speaker: Speaker) => {
     setSelectedSpeaker(speaker);
@@ -25,23 +20,21 @@ export default function Slider({ speakers }: { speakers: Speaker[] }) {
 
   return (
     <>
-      <div className="slider-container flex w-max">
+      <div className="slider-container flex w-[100%]">
         <div
-          className={` ${selectedSpeaker ? "paused" : "animate-scroll-infinite"} space-x-8`}
+          className={`${selectedSpeaker ? "paused" : "animate-scroll-infinite"}`}
         >
           {speakersLoop.map((speaker, index) => (
             <div
               key={index}
               onClick={() => handleOpenModal(speaker)}
-              style={cardImage(speaker.image)}
-              className="speaker-card aspect-[12/16] min-w-[300px] cursor-pointer bg-cover bg-center p-4 md:min-w-[350px]"
+              className="speaker-card flex min-w-[50%] items-center justify-center md:min-w-[25%]"
             >
               <SpeakerCard speaker={speaker} />
             </div>
           ))}
         </div>
       </div>
-
       {selectedSpeaker && (
         <Modal
           isOpen={viewModal}
