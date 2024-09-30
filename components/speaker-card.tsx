71b51f77b5efdc037cec9bd523b31531639364d5
Christopher Juwao <njuwao@icloud.com>
2024-09-30 16:03:07 +0200

    speaker card

--- components/speaker-card.tsx
+++ components/speaker-card.tsx
@@ -1,19 +1,28 @@
 import { Speaker } from "@/types";
 
-export default function SliderCard({ speaker }: { speaker: Speaker }) {
+export default function SpeakerCard({ speaker }: { speaker: Speaker }) {
+  const cardImage = (url: string): React.CSSProperties => ({
+    backgroundImage: `url(${url})`,
+  });
+
   return (
-    <div className="flex h-full flex-col justify-end border border-green-500">
-      <h2 className="text-4xl font-bold text-white">{speaker.name}</h2>
-      <h3 className="text-3xl font-bold text-white">{speaker.company}</h3>
-      <div className="flex space-x-1 py-3 md:space-x-4">
-        {speaker.topics.map((topic) => (
-          <div
-            key={topic}
-            className="flex items-center justify-center rounded-full border border-white bg-white/10 px-2 text-xl font-medium text-white"
-          >
-            <span>{topic}</span>
-          </div>
-        ))}
+    <div
+      style={cardImage(speaker.image)}
+      className="aspect-[12/16] min-w-[90%] bg-cover bg-center"
+    >
+      <div className="flex h-full flex-col justify-end">
+        <h2 className="text-3xl font-bold text-white">{speaker.name}</h2>
+        <h3 className="text-2xl font-bold text-white">{speaker.company}</h3>
+        <div className="flex">
+          {speaker.topics.map((topic) => (
+            <div
+              key={topic}
+              className="border-white-500 flex min-w-[20%] items-center justify-center rounded-full border p-2"
+            >
+              <div className="text-md text-white">{topic}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
