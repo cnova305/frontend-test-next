592c7118941c7b02b491267c70942e1a09018cfd
Christopher Juwao <njuwao@icloud.com>
2024-09-30 13:42:37 +0200

    slider skeleton

--- components/slider-skeleton.tsx
+++ components/slider-skeleton.tsx
@@ -2,7 +2,7 @@ import { IoWarning } from "react-icons/io5";
 
 function SliderError() {
   return (
-    <div className="error-bg-color error-color flex aspect-[12/16] min-w-[80%] flex-col items-center justify-center rounded-lg border">
+    <div className="error-color flex aspect-[12/16] min-w-[80%] flex-col items-center justify-center rounded-lg border border-green-500">
       <IoWarning size={150} />
       <div className="text-xl font-bold">Failed to load</div>
     </div>
@@ -15,7 +15,7 @@ export default function SliderSkeleton() {
         {Array.from({ length: 4 }, (_, index) => (
           <div
             key={index}
-            className="flex min-w-[70%] items-center justify-center md:min-w-[25%]"
+            className="flex min-w-[70%] items-center justify-center border md:min-w-[25%]"
           >
             <SliderError />
           </div>
