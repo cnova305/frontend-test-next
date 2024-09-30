2301828273d7da680fff01157e772816a76f8594
Christopher Juwao <njuwao@icloud.com>
2024-09-30 12:21:41 +0200

    slider skeleton

--- components/slider-skeleton.tsx
+++ components/slider-skeleton.tsx
@@ -1,18 +1,26 @@
-import { IoPerson } from "react-icons/io5";
-import CircularProgress from "./circular-progress";
+import { IoWarning } from "react-icons/io5";
 
+function SliderError() {
+  return (
+    <div className="error-bg-color error-color flex aspect-[12/16] min-w-[80%] flex-col items-center justify-center rounded-lg border">
+      <IoWarning size={150} />
+      <div className="text-xl font-bold">Failed to load</div>
+    </div>
+  );
+}
 export default function SliderSkeleton() {
   return (
-    <div className="slider-container flex w-max justify-between">
-      {Array.from({ length: 4 }, (_, index) => (
-        <div
-          key={index}
-          className="flex aspect-[12/16] min-w-[375px] flex-col items-center justify-center p-10 shadow-xl"
-        >
-          <IoPerson color="gray" size={200} />
-          <CircularProgress />
-        </div>
-      ))}
+    <div className="slider-container flex w-[100%]">
+      <div className="array-container">
+        {Array.from({ length: 4 }, (_, index) => (
+          <div
+            key={index}
+            className="flex min-w-[70%] items-center justify-center md:min-w-[25%]"
+          >
+            <SliderError />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
